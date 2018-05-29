0911cbe9a4c52629c480d8b7f37cd3cae379b996
unknown <ddimasss@mail.ru>
2018-05-29 23:01:28 +0200

    fix

--- lm/models.py
+++ lm/models.py
@@ -1,15 +1,27 @@
 from django.db import models
-from django.conf import settings
+
 
 
 # Create your models here.
 
 class Lm(models.Model):
+  lm_types = (
+    (0, 'Regresions'),
+    (1, 'Classification'),
+  )
+
+  lm_libs = (
+    (0, 'Scikit-learn'),
+    (1, 'Tensorflow'),
+    (2, 'Keras'),
+  )
   name = models.CharField(max_length=200)
-  desc = models.DateTimeField('date published')
+  desc = models.TextField()
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
   image = models.FileField(upload_to='uploads/%Y/%m/%d/')
-  lm_type = models.IntegerField(choices=settings.lm_types)
-  lm_lib = models.IntegerField(choices=settings.lm_libs)
-  attrs = models.JSONField()
+  lm_type = models.IntegerField(choices=lm_types)
+  lm_lib = models.IntegerField(choices=lm_libs)
+  def __str__(self):
+    return self.name
+
